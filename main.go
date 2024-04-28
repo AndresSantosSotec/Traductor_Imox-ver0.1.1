624b0405c2b4e29ebc69f2bd84925fc3a042e8a7
AndresDev <mlpdbz300@gmail.com>
2024-04-27 23:16:12 -0600

    Cambios

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-// main.go
-
 package main
 
 import (
@@ -13,14 +11,20 @@ var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 func main() {
 	http.HandleFunc("/", Inicio)
 	http.HandleFunc("/agregarpalabra", agregarpalabra)
-	log.Println("Servidor corriendo ")
+	http.HandleFunc("/add", abrirAddTemplate)
+	log.Println("Servidor corriendo en el puerto 8080")
 	http.ListenAndServe(":8080", nil)
 }
 
 func Inicio(w http.ResponseWriter, r *http.Request) {
-	plantillas.ExecuteTemplate(w, "inicio", nil)
+	// Renderizar el formulario de inicio de sesión
+	plantillas.ExecuteTemplate(w, "login", nil)
 }
 
 func agregarpalabra(w http.ResponseWriter, r *http.Request) {
-	plantillas.ExecuteTemplate(w, "agregarpalabra", nil) // Corregir el nombre de la plantilla aquí
+	plantillas.ExecuteTemplate(w, "agregarpalabra", nil)
+}
+
+func abrirAddTemplate(w http.ResponseWriter, r *http.Request) {
+	plantillas.ExecuteTemplate(w, "add", nil)
 }
